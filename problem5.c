428713f099302cc4bf6e75f7438078b65b5842be
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-05-07 17:25:49 +0600

    Problem 5 solved

---
+++ problem5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void count(int n){
+    static int d = 1;
+    printf("%d ", n);
+    printf("%d ", d);
+    d++;
+    if(n>1)
+        count(n - 1);
+    printf("%d ", d);
+}
+int main()
+{
+    count(3);
+    return 0;
+}
+
+// Answer 3 1 2 2 1 3 4 4 4 ;
